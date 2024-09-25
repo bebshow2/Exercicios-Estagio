df87c575554e7b2fb37c72e422b190e057e8621d
Guilherme <g.cmedeiros@hotmail.com>
2024-09-25 17:30:07 -0300

    finalizado exercicios

--- tarefa 2/fibonacci.js
+++ tarefa 2/fibonacci.js
@@ -10,19 +10,16 @@ function pertenceAFibonacci(num) {
     let a = 0;
     let b = 1;
 
-    // Verifica se o número é 0 ou 1 (casos base)
     if (num === a || num === b) {
         return true;
     }
 
-    // Gera a sequência de Fibonacci até o número informado
     while (b < num) {
         const proximo = a + b;
         a = b;
         b = proximo;
     }
 
-    // Retorna se o número pertence ou não
     return b === num;
 }
 
